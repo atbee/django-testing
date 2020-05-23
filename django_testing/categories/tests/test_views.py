a3f5ef475b4044838d83485d152cf50b51b17ac3
Athibet Prawane <atb@odds.team>
2020-05-23 11:57:21 +0700

    Use assertion from pytest :bee:

--- django_testing/categories/tests/test_views.py
+++ django_testing/categories/tests/test_views.py
@@ -16,3 +16,5 @@ class TestCateforyList(TestCase):
         # Then
         self.assertContains(response, '<li>Street Food</li>')
         self.assertContains(response, '<li>Elite</li>')
+
+        assert '<li>Street Food</li>' in str(response.content)
